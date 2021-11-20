c12cf89abf520de8cbb373140d1860089eb492b1
Nick Tomlin <NickTomlin@users.noreply.github.com>
2021-11-20 13:24:17 -0600

    chore: Update module

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
+import { greet } from "https://deno.land/x/ntomlin_example@1.0.1/index.ts"
 
 function handler(req) {
-  return new Response("Hello world, from github!");
+  const message = greet("Hardcoded name")
+  return new Response(message);
 }
 
 console.log("Listening on http://localhost:8000");
