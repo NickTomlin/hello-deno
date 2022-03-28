705667a26c8f1ba046e6df5ca25f7e6ff747b4c6
Nick Tomlin <NickTomlin@users.noreply.github.com>
2022-03-28 15:39:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
 import { greet } from "https://deno.land/x/ntomlin_example@1.0.1/index.ts"
 
 function handler(req) {
-  const message = greet("Hardcoded name")
+  const message = greet("Hardcoded name!!!")
   return new Response(message);
 }
 
